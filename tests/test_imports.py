05cda4493b6e9457ac9f53f0edc21f8e6ab74e19
Jan Petter Maehlen <jepe@ife.no>
2020-10-18 13:08:18 +0200

    update

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,9 +1,8 @@
-import pytest
-import cellpy_utils as cutil
+from cellpy_utils import gutils
 
 
 def test_minimal():
     a = 2
-    b = cutil.double(a)
+    b = gutils.double(a)
     print(f"{a} -> {b}")
 
